50b27c572609539c54841100cdc5664633e29b2b
Brad Jones <BJ2044@gmail.com>
2022-11-10 17:04:38 -0500

    React w/Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const me: Person = {
 
 const card: PhotoCardType = {
   photo: "https://lh3.googleusercontent.com/ogw/AOh-ky34t5fd0w9ldRPKMVEIsV_xbcSYNHplZ9kVirThwQ=s32-c-mo",
-  user: "Todd Albert",
+  user: "BJ2044",
   mainPhoto: 'https://images.unsplash.com/photo-1665398892998-8ee81dae4bcb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODExNjY5Mg&ixlib=rb-1.2.1&q=80&w=1080',
   description: 'Render of overlapping brick balconies with shrubs growing on each',
   likeCount: 99
@@ -25,7 +25,7 @@ function App() {
         <h1>Hello TypeScript 👋🏼</h1>
         <AboutMe person={me}/>
         <Counter />
-        <Greeting firstName='Tyler' lastName='Jefferson' />
+        <Greeting firstName='Brad' lastName='Jones' />
         <Greeting firstName={'Lazy'} lastName={'Sloth'} />
         <PhotoCard card={card} />
       </header>
